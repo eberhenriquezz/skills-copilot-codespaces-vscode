8dd8d5363db433c37455cda382bc59301dddda12
eber <133610237+eberhenriquezz@users.noreply.github.com>
2023-07-30 14:54:17 +0000

    ccommit

--- member.js
+++ member.js
@@ -1,1 +1,5 @@
-function skillsMember()
+function skillsMember() {
+  // Here, we're using the "this" keyword to refer to the object that this function is a part of
+  // This is the same as saying "var1 + var2"
+  return this.strength + this.agility + this.intelligence;
+}
